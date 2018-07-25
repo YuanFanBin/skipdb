489d5d82cac989fbf268b5ac842f035f057a5b9f
YuanFanBin <yuanfanbin@gmail.com>
2018-07-25 18:19:39 +0800

    add comment

--- include/skiplist.h
+++ include/skiplist.h
@@ -15,10 +15,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define METANODE_HEAD 0x8000 // 跳表头节点
+#define METANODE_HEAD 0x8000    // 跳表头节点
 #define METANODE_DELETED 0x0002 // 跳表节点已被惰性删除
-#define METANODE_USED 0x0001 // 跳表节点已被使用
-#define METANODE_NONE 0x0000 // 空节点(未被使用过)
+#define METANODE_USED 0x0001    // 跳表节点已被使用
+#define METANODE_NONE 0x0000    // 空节点(未被使用过)
 
 // 元数据文件最大大小（不自动扩容）
 // 最大可容纳：4 * 1024 * 1024 / (sizeof(metanode) + sizeof(uint64_t) = 104857个key
@@ -31,59 +31,60 @@
 #define SKIPLIST_MAXLEVEL   64      // 跳表最大level
 
 #define SKIPLIST_STATE_NORMAL 1
-#define SKIPLIST_STATE_DEFRAG 2
-#define SKIPLIST_STATE_SPLITED 3
-#define SKIPLIST_STATE_SPLITER 4
-#define SKIPLIST_STATE_MERGE_LOG 5
+#define SKIPLIST_STATE_DEFRAG 2     // 跳表data文件碎片整理
+#define SKIPLIST_STATE_SPLITED 3    // 被分裂者
+#define SKIPLIST_STATE_REDO_LOG 4   // redo log
+#define SKIPLIST_STATE_SPLITER 5    // 分裂者（left, right)
+#define SKIPLIST_STATE_MERGE_LOG 6  // 分裂者合并redo log
 
 typedef struct metanode_s {
-    uint32_t level;
-    uint32_t flag;
-    uint64_t offset;
-    uint64_t value;
-    uint64_t backward;
-    uint64_t forwards[0];
+    uint32_t level;       // 当前节点高度
+    uint32_t flag;        // 元数据节点状态：METANODE_XXXX
+    uint64_t offset;      // key数据存储在data文件中的偏移量
+    uint64_t value;       // value值
+    uint64_t backward;    // 跳表节点指向的前置节点偏移量
+    uint64_t forwards[0]; // 跳表节点只想的后置节点偏移量，forwards个数由level决定
 } metanode_t;
 
 typedef struct skipmeta_s {
-    uint64_t mapsize; // 已使用used
-    uint64_t mapcap;  // 已映射total
-    uint64_t tail;    // tail metanode
+    uint64_t mapsize; // 元数据文件已使用字节数
+    uint64_t mapcap;  // 元数据文件映射大小
+    uint64_t tail;    // 最后一个元数据在元数据文件中的起始偏移量
     uint32_t count;   // key个数（不包括已被删除节点）
     float p;          // p
-    void* mapped;     // mmap map pointer
+    void* mapped;     // 映射起始地址
 } skipmeta_t;
 
 typedef struct datanode_s {
-    uint64_t offset;
-    uint16_t size; // NOTE: key max
-    void* data[0];
+    uint64_t offset; // 数据节点指向的元数据节点在meta文件中的偏移量
+    uint16_t size;   // 数据节点data大小
+    void* data[0];   // data内容（key）
 } datanode_t;
 
 typedef struct skipdata_s {
-    uint64_t mapsize;
-    uint64_t mapcap;
-    void* mapped;
+    uint64_t mapsize;   // 数据文件已使用字节数
+    uint64_t mapcap;    // 数据文件映射大小
+    void* mapped;       // 映射起始地址
 } skipdata_t;
 
 struct skipsplit_s;
 
 typedef struct skiplist_s {
     pthread_rwlock_t rwlock;
-    skipmeta_t* meta;
-    skipdata_t* data;
-    list_t* metafree[SKIPLIST_MAXLEVEL];
-    list_t* datafree;
-    char* metaname;
-    char* dataname;
-    struct skipsplit_s* split;
-    int state;
+    skipmeta_t* meta;                    // 元数据跳表
+    skipdata_t* data;                    // 数据跳表
+    list_t* metafree[SKIPLIST_MAXLEVEL]; // 空闲元数据节点（仅存偏移量）
+    list_t* datafree;                    // 空闲数据节点（仅存偏移量）
+    char* metaname;                      // 映射的元数据文件名
+    char* dataname;                      // 映射的数据文件名
+    struct skipsplit_s* split;           // 分裂跳表
+    int state;                           // 跳表状态: SKIPLIST_STATE_XXXX
 } skiplist_t;
 
 typedef struct skipsplit_s {
-    skiplist_t* redolog;
-    skiplist_t* left;
-    skiplist_t* right;
+    skiplist_t* redolog;    // 分裂时原始跳表使用的读写跳表：redo log
+    skiplist_t* left;       // 原始跳表分裂成的左半步分跳表
+    skiplist_t* right;      // 原始跳表分裂成的有半步分跳表
 } skipsplit_t;
 
 status_t sl_open(const char* prefix, float p, skiplist_t** sl);
