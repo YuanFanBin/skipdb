cecc13de80917584b5e388ce86d3b9433632d7b6
fanzhaobing <fanzhaobing@tulong.com>
2018-07-30 15:20:23 +0800

    修改 status_t

--- include/status.h
+++ include/status.h
@@ -6,11 +6,13 @@
 #define STATUS_SKIPLIST_FULL 1
 #define STATUS_SKIPLIST_LOAD 2
 
+#define STATUS_SKIPDB_CLOSED 3
+#define STATUS_SKIPDB_NOTFOUND 4
+
 #define STATUS_SSL_LAZY_DELETED 100
 
-typedef struct status_s {
-    int ok;
-    int type;
+typedef struct {
+    int errno;
     char errmsg[ERRMSG_SIZE];
 } status_t;
 
