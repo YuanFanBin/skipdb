7aa4a6ad3bd4007e5932a4beed7518c6043718ec
chenchangshuai <chenchangshuai>
2018-07-31 19:10:07 +0800

    bug fix

--- src/btree.c
+++ src/btree.c
@@ -680,7 +680,7 @@ data_type btree_search(btree_t *bt, key_type key) {
     leaf = n;
     slot = find_lower_leaf(leaf, key);
     data_type value = (slot < leaf->slotuse && key_equal(key, leaf->keyslots[slot]))
-           ? leaf->dataslots[slot] : NULL;
+           ? leaf->dataslots[slot] : bt->ftail;
     pthread_rwlock_unlock(&(bt->rwlock));
 }
 
