82cd14cbd22236a1aa25f11c38875530a7fa906a
fanzhaobing <fanzhaobing@tulong.com>
2018-08-01 19:01:56 +0800

    bug fixed

--- tester/skipdb_main.c
+++ tester/skipdb_main.c
@@ -4,60 +4,61 @@
 #include "tester.h"
 
 int put(void *db, const char *key, int key_len, uint64_t value) {
-  status_t st = skipdb_put(db, key, (size_t)key_len, value);
-  if (st.code != 0) {
-    printf("skipdb_put -- st.code %d, errmsg: %s\n\tkey: %s, key_len: %d\n",
-           st.code, st.errmsg, key, key_len);
-  }
-  return st.code;
+    status_t st = skipdb_put(db, key, (size_t) key_len, value);
+    if (st.code != 0) {
+        printf("skipdb_put -- st.code %d, errmsg: %s\n\tkey: %s, key_len: %d\n",
+               st.code, st.errmsg, key, key_len);
+    }
+    return st.code;
 }
 
 int main(int argc, const char *argv[]) {
-  if (argc <= 1) {
-    printf("Usage: ./a.out <count> <key_len>\n");
-    exit(-1);
-  }
+    if (argc <= 1) {
+        printf("Usage: ./a.out <count> <key_len>\n");
+        exit(-1);
+    }
 
-  int count = atoi(argv[1]);
-  if (count < 0) {
-    printf("Error: `count` must >= 0\n");
-    exit(-1);
-  }
+    int count = atoi(argv[1]);
+    if (count < 0) {
+        printf("Error: `count` must >= 0\n");
+        exit(-1);
+    }
 
-  int key_len = atoi(argv[2]);
-  if (key_len < 0) {
-    printf("Error: `key_len` must >= 0\n");
-    exit(-1);
-  }
-  printf("count: %d, key_len: %d\n\n", count, key_len);
+    int key_len = atoi(argv[2]);
+    if (key_len < 0) {
+        printf("Error: `key_len` must >= 0\n");
+        exit(-1);
+    }
+    printf("count: %d, key_len: %d\n\n", count, key_len);
 
-  status_t st = {0};
-  skipdb_t *db = NULL;
+    status_t st = {0};
+    skipdb_t *db = NULL;
 
-  st = skipdb_open("./data", &db, NULL);
-  if (st.code != 0) {
-    panic("skipdb_open(st.code not 0)");
-  }
+    st = skipdb_open("./data", &db, NULL);
+    if (st.code != 0) {
+        panic("skipdb_open(st.code not 0)");
+    }
 
-  test_t t;
-  t.db = db;
-  t.put = put;
+    test_t t;
+    t.db = db;
+    t.put = put;
 
-  t.dis.count = count;
-  t.dis.dis_items_len = 1;
-  t.dis.dis_items[0].key_len = key_len;
-  t.dis.dis_items[0].percent = 100;
+    t.dis.count = count;
+    t.dis.dis_items_len = 1;
+    t.dis.dis_items = malloc(sizeof(dis_item_t) * t.dis.dis_items_len);
+    t.dis.dis_items[0].key_len = key_len;
+    t.dis.dis_items[0].percent = 100;
 
-  int r = benchmark_put(t);
-  if (r != 0) {
-    printf("status.code: %d\n", r);
-    panic("benchmark_put(return value not 0)");
-  }
+    int r = benchmark_put(t);
+    if (r != 0) {
+        printf("status.code: %d\n", r);
+        panic("benchmark_put(return value not 0)");
+    }
 
-  st = skipdb_close(db);
-  if (st.code != 0) {
-    panic("skipdb_close(st.code not 0)");
-  }
+    st = skipdb_close(db);
+    if (st.code != 0) {
+        panic("skipdb_close(st.code not 0)");
+    }
 
-  return 0;
+    return 0;
 }
--- tester/tester.c
+++ tester/tester.c
@@ -17,99 +17,99 @@
 // 复合测试
 
 void randString(int len, char *str) {
-  char *_str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  size_t _len = strlen(_str);
+    char *_str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    size_t _len = strlen(_str);
 
-  for (int i = 0; i < len - 1; ++i) {
-    str[i] = _str[random() % _len];
-  }
-  str[len - 1] = '\0';
+    for (int i = 0; i < len - 1; ++i) {
+        str[i] = _str[random() % _len];
+    }
+    str[len - 1] = '\0';
 }
 
 void assert_dis(dis_t d) {
-  assert(d.count > 0);
-  assert(d.dis_items_len > 0);
+    assert(d.count > 0);
+    assert(d.dis_items_len > 0);
 
-  int sum = 0;
-  for (int i = 0; i < d.dis_items_len; ++i) {
-    sum += d.dis_items[i].percent;
-  }
-  assert(sum == 100);
+    int sum = 0;
+    for (int i = 0; i < d.dis_items_len; ++i) {
+        sum += d.dis_items[i].percent;
+    }
+    assert(sum == 100);
 }
 
 char **make_data(dis_t d) {
-  assert_dis(d);
+    assert_dis(d);
 
-  char **data = malloc(sizeof(char *) * d.count);
+    char **data = malloc(sizeof(char *) * d.count);
 
-  int dd[100] = {0};
-  int dd_ix = 0;
-  for (int i = 0; i < d.dis_items_len; ++i) {
-    for (int j = 0; j < d.dis_items[i].percent; ++j) {
-      dd[dd_ix++] = d.dis_items[i].key_len;
+    int dd[100] = {0};
+    int dd_ix = 0;
+    for (int i = 0; i < d.dis_items_len; ++i) {
+        for (int j = 0; j < d.dis_items[i].percent; ++j) {
+            dd[dd_ix++] = d.dis_items[i].key_len;
+        }
     }
-  }
 
-  // 随机分布
-  for (int i = 0; i < d.count; ++i) {
-    int key_len = dd[random() % 100];
-    data[i] = malloc(key_len + 1);
+    // 随机分布
+    for (int i = 0; i < d.count; ++i) {
+        int key_len = dd[random() % 100];
+        data[i] = malloc(key_len + 1);
 
-    randString(key_len + 1, data[i]);
-  }
+        randString(key_len + 1, data[i]);
+    }
 
-  return data;
+    return data;
 }
 
 void free_data(dis_t d, char **data) {
-  for (int i = 0; i < d.count; ++i) {
-    free(data[i]);
-  }
-  free(data);
+    for (int i = 0; i < d.count; ++i) {
+        free(data[i]);
+    }
+    free(data);
 }
 
 double delta(struct timeval t1, struct timeval t2) {
-  return ((double)t2.tv_sec + (double)t2.tv_usec / 1000000) -
-         ((double)t1.tv_sec + (double)t1.tv_usec / 1000000);
+    return ((double) t2.tv_sec + (double) t2.tv_usec / 1000000) -
+           ((double) t1.tv_sec + (double) t1.tv_usec / 1000000);
 }
 
 // 固定长度 32
 int benchmark_put(test_t t) {
-  int ret = 0;
-  dis_t dis = t.dis;
+    int ret = 0;
+    dis_t dis = t.dis;
 
-  char **data = make_data(dis);
+    char **data = make_data(dis);
 
-  for (size_t i = 0; i < dis.count; i++) {
-    if (strlen(data[i]) != 32) {
-      printf("data[%ld]: %ld\n", i, strlen(data[i]));
-    }
-  }
-
-  struct timeval t1, t2;
-
-  {
-    gettimeofday(&t1, NULL);
-
-    for (int i = 0; i < dis.count; ++i) {
-      if ((ret = t.put(t.db, data[i], (int)strlen(data[i]), (uint64_t)i)) !=
-          0) {
-        return ret;
-      }
+    for (size_t i = 0; i < dis.count; i++) {
+        if (strlen(data[i]) != 32) {
+            printf("data[%ld]: %ld\n", i, strlen(data[i]));
+        }
     }
 
-    gettimeofday(&t2, NULL);
-  }
+    struct timeval t1, t2;
 
-  double timed = delta(t1, t2);
-  printf("PUT time: %lf, %lfw/s\n", timed, dis.count / timed / 10000);
+    {
+        gettimeofday(&t1, NULL);
 
-  free_data(dis, data);
-  return ret;
+        for (int i = 0; i < dis.count; ++i) {
+            if ((ret = t.put(t.db, data[i], (int) strlen(data[i]), (uint64_t) i)) !=
+                0) {
+                return ret;
+            }
+        }
+
+        gettimeofday(&t2, NULL);
+    }
+
+    double timed = delta(t1, t2);
+    printf("PUT time: %lf, %lfw/s\n", timed, dis.count / timed / 10000);
+
+    free_data(dis, data);
+    return ret;
 }
 
 void panic(char *msg) {
-  printf("panic: %s\n", msg);
-  printf("errno: %d, error: %s\n", errno, strerror(errno));
-  exit(-1);
+    printf("panic: %s\n", msg);
+    printf("errno: %d, error: %s\n", errno, strerror(errno));
+    exit(-1);
 }
